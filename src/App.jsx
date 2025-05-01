09e2eb59ac2d7efcd245cdc59f8236b37cdc8951
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2025-05-01 15:15:12 +0700

    merge master

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
-import RootLayout from './layout/RootLayout'
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
+import React from 'react';
+import { Route, Routes } from 'react-router';
+import RootLayout from './layout/RootLayout';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route element={<RootLayout />} >
+        <Route element={<RootLayout />}>
           <Route path="/" element={<Home />} />
         </Route>
 
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/navigation-bar/navbar-search-bar.jsx
+++ src/components/navigation-bar/navbar-search-bar.jsx
@@ -1,20 +1,18 @@
-import { Search } from 'lucide-react'
-import React from 'react'
+import { Search } from 'lucide-react';
+import React from 'react';
 
 const SearchBar = () => {
-	return (
-		<div className="relative">
-			<input
-				placeholder="Search..."
-				className="p-2 rounded-xl w-full border-base-100 text-white"
-			/>
-			<button
-				className="absolute inset-y-0 right-0 flex items-center pr-3"
-			>
-				<Search size={24} />
-			</button>
-		</div>
-	)
-}
+  return (
+    <div className="relative">
+      <input
+        placeholder="Search..."
+        className="p-2 rounded-xl w-full border-base-100 text-white"
+      />
+      <button className="absolute inset-y-0 right-0 flex items-center pr-3">
+        <Search size={24} />
+      </button>
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
