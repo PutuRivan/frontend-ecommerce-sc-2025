7fb8129ca7cfd5f3bd3f0a4b3502a30ebadc626d
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2025-05-01 14:37:33 +0700

    feat:react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import Navbar from './components/navigation-bar/Navbar'
+import { Route, Routes } from 'react-router'
+import RootLayout from './layout/RootLayout'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
     <>
-      <div className=''>
-        <Navbar/>
-      </div>
-    </>  
-    )
+      <Routes>
+        <Route element={<RootLayout />} >
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
