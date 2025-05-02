65b9e4d8737083108b1b86545881071017ee3919
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2025-05-02 22:25:25 +0700

    feat:home page

---
+++ src/components/home/filter-product-category.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {
+	Select,
+	SelectTrigger,
+	SelectValue,
+	SelectContent,
+	SelectItem,
+} from "../ui/Select"
+
+const FilterProductCategory = () => {
+	return (
+		<div className='flex w-1/4 items-center justify-center p-10'>
+			<Select>
+				<SelectTrigger>
+					<SelectValue placeholder="Pilih kategori" />
+				</SelectTrigger>
+				<SelectContent>
+					<SelectItem value="frontend">Frontend</SelectItem>
+					<SelectItem value="backend">Backend</SelectItem>
+					<SelectItem value="fullstack">Fullstack</SelectItem>
+				</SelectContent>
+			</Select>
+		</div>
+	)
+}
+
+export default FilterProductCategory
---
+++ src/components/ui/card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Card = () => {
+	return (
+		<div className="card bg-base-300 w-96 shadow-sm">
+			<figure>
+				<img
+					src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+					alt="Shoes" />
+			</figure>
+			<div className="card-body">
+				<h2 className="card-title">Nama Product</h2>
+				<p>Harga</p>
+				<div className="card-actions justify-end">
+					<button className="btn btn-primary">Buy Now</button>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Card
