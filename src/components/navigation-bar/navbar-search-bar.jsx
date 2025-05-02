0f0c0743c034e25c61afd5866e29cfdb82f64875
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2025-05-02 23:18:20 +0700

    style:cursor pointer on search button

--- src/components/navigation-bar/navbar-search-bar.jsx
+++ src/components/navigation-bar/navbar-search-bar.jsx
@@ -24,7 +24,7 @@ const SearchBar = () => {
         ref={ref}
         onKeyDown={handleSearch}
       />
-      <button onClick={handleSearch} className="absolute inset-y-0 right-0 flex items-center pr-3">
+      <button onClick={handleSearch} className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer">
         <Search size={24} />
       </button>
     </div>
