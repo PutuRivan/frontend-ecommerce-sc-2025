ae1a24315314f4b203e2f714212c23b37e3aaef6
Putu Rivan <150578221+PutuRivan@users.noreply.github.com>
2025-05-02 23:13:32 +0700

    Feat/navigation bar (#6)

--- src/components/navigation-bar/navbar-profile.jsx
+++ src/components/navigation-bar/navbar-profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const NavbarProfile = () => {
   return (
@@ -20,16 +21,12 @@ const NavbarProfile = () => {
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
       >
         <li>
-          <a className="justify-between">
+          <Link to="/profile" className="justify-between">
             Profile
-            <span className="badge">New</span>
-          </a>
+          </Link>
         </li>
         <li>
-          <a>Settings</a>
-        </li>
-        <li>
-          <a>Logout</a>
+          <button>Logout</button>
         </li>
       </ul>
     </div>
--- src/components/navigation-bar/navbar-search-bar.jsx
+++ src/components/navigation-bar/navbar-search-bar.jsx
@@ -1,14 +1,30 @@
 import { Search } from 'lucide-react';
-import React from 'react';
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router';
 
 const SearchBar = () => {
+  const ref = useRef();
+  const navigate = useNavigate();
+  const handleSearch = (e) => {
+    const keyword = ref.current.value;
+
+    if (!keyword || keyword.trim() === '') return;
+
+    if (e.key === 'Enter' || e.type === 'click') {
+      e.preventDefault();
+      navigate(`/product/${keyword}`);
+      ref.current.value = '';
+    }
+  };
   return (
     <div className="relative">
       <input
         placeholder="Search..."
         className="p-2 rounded-xl w-full border-base-100 text-white"
+        ref={ref}
+        onKeyDown={handleSearch}
       />
-      <button className="absolute inset-y-0 right-0 flex items-center pr-3">
+      <button onClick={handleSearch} className="absolute inset-y-0 right-0 flex items-center pr-3">
         <Search size={24} />
       </button>
     </div>
