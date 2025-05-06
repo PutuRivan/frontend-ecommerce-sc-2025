d0ed89d45130d22a1889574c63dca0a9d4b2539d
Putu Rivan <150578221+PutuRivan@users.noreply.github.com>
2025-05-06 23:41:13 +0700

    feat:protected middlewares (#14)

---
+++ src/middlewares/protected-routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useAuth } from '../context/auth-context'
+import { Navigate, Outlet } from 'react-router'
+
+const ProtectedRoutes = ({ allowedRoles }) => {
+    const { user, isAuthenticated } = useAuth()
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace />
+    }
+
+    if (!allowedRoles.includes(user?.level)) {
+        return <Navigate to="/" replace />
+    }
+
+    return <Outlet />
+}
+
+export default ProtectedRoutes
